e2f35b827f6cd9b7d0220ac8de0c8604c0f3492b
Vincent Grossier <vincent.grossier@gmail.com>
2022-06-04 19:55:56 +0200

    Initial Commit

---
+++ Paypaal/PaypaalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaypaalApp.swift
+//  Paypaal
+//
+//  Created by Vincent Grossier on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaypaalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
